9b1ad3fe21e4ab7e7108450e651a30d55fcf5f73
SycaTech <148126462+SycaTech@users.noreply.github.com>
2026-01-18 17:07:17 +0200

    hk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Shooter.java
@@ -9,14 +9,19 @@ import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 
 import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion;
+import org.firstinspires.ftc.teamcode.pedroPathing.follower.Follower;
 
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.utility.InstantCommand;
 import dev.nextftc.core.subsystems.Subsystem;
-
+import dev.nextftc.hardware.controllable.MotorGroup;
+import dev.nextftc.hardware.controllable.RunToVelocity;
 
 public class Shooter implements Subsystem {
     public static final Shooter INSTANCE = new Shooter();
     private Shooter() {}
-    public DcMotorEx shootermotor;
+    public DcMotorEx Master;
+    public DcMotorEx Slave;
     public double highvelocity = 1500;
     public double lowvelocity = 900;
 
@@ -24,18 +29,32 @@ public class Shooter implements Subsystem {
     double P = 0;
     double F = 0;
 
-    double[] stepsizes = {10.0, 1.0, 0.1, 0.01, 0.0001};
+    double[] stepsizes = {10.0, 1.0, 0.1, 0.001, 0.0001};
     int stepIndex = 1;
 
         public void init(HardwareMap hardwareMap) {
-            shootermotor = hardwareMap.get(DcMotorEx.class, "shoot");
+            Master = hardwareMap.get(DcMotorEx.class, "Master");
+            Slave = hardwareMap.get(DcMotorEx.class, "Slave");
 
+            Master.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            Master.setDirection(DcMotorSimple.Direction.REVERSE);
+            Slave.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            Slave.setDirection(DcMotorSimple.Direction.FORWARD);
 
-        shootermotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        shootermotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P, 0, 0, F);
+            Master.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+            Slave.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
 
-        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P, 0, 0, F);
-        shootermotor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER,pidfCoefficients);
-
+        }
+        public Command ppp(){
+            return new InstantCommand(()-> Master.setVelocity(1)).and
+                    (new InstantCommand(()-> Slave.setVelocity(1)));
+    }
+        public Command shoot(){
+                return new InstantCommand(()-> Master.setVelocityPIDFCoefficients(1,0,0,0))
+                        .and(new InstantCommand(()-> Slave.setVelocityPIDFCoefficients(1,0,0,0)));
+        }
+        public void loop(){
+            Master.setPower(Slave.getPower());
         }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ShooterTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ShooterTeleop.java
@@ -1,7 +1,8 @@
-package org.firstinspires.ftc.teamcode.Subsystems;
+
+        package org.firstinspires.ftc.teamcode.Subsystems;
 
 
-import android.widget.Button;
+import static dev.nextftc.bindings.Bindings.button;
 
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
@@ -11,6 +12,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.Subsystems.Shooter;
 
+import dev.nextftc.bindings.Button;
 import dev.nextftc.core.components.BindingsComponent;
 import dev.nextftc.core.components.SubsystemComponent;
 import dev.nextftc.ftc.Gamepads;
@@ -18,7 +20,6 @@ import dev.nextftc.ftc.NextFTCOpMode;
 import dev.nextftc.ftc.components.BulkReadComponent;
 
 @TeleOp(name = "ShooterTeleop")
-
 
 
 public class ShooterTeleop extends NextFTCOpMode {
@@ -31,9 +32,10 @@ public class ShooterTeleop extends NextFTCOpMode {
         );
     }
 
-    public DcMotorEx shootermotor;
-    double highVelocity;
+   public DcMotorEx Master;
+    public DcMotorEx Slave;
     double lowVelocity;
+    double highVelocity;
     double curTargetVelocity;
     double F;
     double P;
@@ -45,10 +47,18 @@ public class ShooterTeleop extends NextFTCOpMode {
     public void onInit() {
         Shooter.INSTANCE.init(hardwareMap);
 
-        shootermotor = Shooter.INSTANCE.shootermotor;
+        Master = Shooter.INSTANCE.Master;
         highVelocity = Shooter.INSTANCE.highvelocity;
         lowVelocity = Shooter.INSTANCE.lowvelocity;
-        curTargetVelocity = Shooter.INSTANCE.highvelocity;
+        curTargetVelocity = Shooter.INSTANCE.curTargetvelocity;
+        F = Shooter.INSTANCE.F;
+        P = Shooter.INSTANCE.P;
+        stepSizes = Shooter.INSTANCE.stepsizes;
+        stepIndex = Shooter.INSTANCE.stepIndex;
+        Slave = Shooter.INSTANCE.Slave;
+        highVelocity = Shooter.INSTANCE.highvelocity;
+        lowVelocity = Shooter.INSTANCE.lowvelocity;
+        curTargetVelocity = Shooter.INSTANCE.curTargetvelocity;
         F = Shooter.INSTANCE.F;
         P = Shooter.INSTANCE.P;
         stepSizes = Shooter.INSTANCE.stepsizes;
@@ -59,35 +69,49 @@ public class ShooterTeleop extends NextFTCOpMode {
 
     @Override
     public void onUpdate() {
+        Button a = new Button(()-> gamepad1.a);
+        a.whenTrue(Shooter.INSTANCE.ppp());
 
-        Gamepads.gamepad1().dpadLeft()
-                .whenBecomesTrue(() -> F -= stepSizes[stepIndex]);
+//        if (gamepad1.a) {
+//            if (curTargetVelocity == highVelocity) {
+//                curTargetVelocity = lowVelocity;
+//            } else {curTargetVelocity = highVelocity;}
+//        }
+//
+//        if (gamepad1.a) {
+//            stepIndex = (stepIndex + 1) % stepSizes.length;
+//        }
+//
+//
+//
+//        if (gamepad1.a) {
+//            P += stepSizes[stepIndex];
+//        }
+//
+//        if (gamepad1.a) {
+//            P += stepSizes[stepIndex];
+//        }
+//
+//
+//
 
-        Gamepads.gamepad1().dpadRight()
-                .whenBecomesTrue(() -> F += stepSizes[stepIndex]);
+        final double TICKS_PER_REVOLUTION = 1200;
 
-        Gamepads.gamepad1().dpadUp()
-                .whenBecomesTrue(() -> P += stepSizes[stepIndex]);
-
-        Gamepads.gamepad1().dpadDown()
-                .whenBecomesTrue(() -> P -= stepSizes[stepIndex]);
-        Gamepads.gamepad1().a()
-                .whenBecomesTrue(() -> shootermotor.setPower(0));
-
-        final double TICKS_PER_REVOLUTION = 103.6;
-
-        double velocityTPS = shootermotor.getVelocity();
+        double velocityTPS = Master.getVelocity();
 
         double velocityRPM = (velocityTPS / TICKS_PER_REVOLUTION) * 60.0;
 
         // set new PIDF coefficients
         PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P, 0, 0, F);
-        shootermotor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+        Master.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+        Slave.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
 
         //set velocity
-        shootermotor.setVelocity(curTargetVelocity);
+        Master.setVelocity(curTargetVelocity);
+        Slave.setVelocity(curTargetVelocity);
 
-        double curVelocity = shootermotor.getVelocity();
+        double curVelocity = Master.getVelocity ();
+        double curVelocty = Slave.getVelocity();
         double error = curTargetVelocity - curVelocity;
 
         telemetry.addData("Target Velocity", curTargetVelocity);
@@ -98,9 +122,4 @@ public class ShooterTeleop extends NextFTCOpMode {
         telemetry.addData("Tuning P", "%.4f (D-Pad U/D", P);
         telemetry.addData("Tuning F", "%.4f (D-Pad L/R)", F);
         telemetry.addData("Step Size", "%.4f (B Button)", stepSizes[stepIndex]);
-        telemetry.update();
-    }
-
-}
-
-
+        telemetry.update(); } }
